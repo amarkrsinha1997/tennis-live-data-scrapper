ebcf92fa25fc1ae1af02b15326b5552e3b9ea389
amarkrsinha1997 <amar17@navgurukul.org>
2018-03-26 23:17:34 +0530

    added functionality to scrape the turn of the player

--- scraper_final.py
+++ scraper_final.py
@@ -2,6 +2,7 @@
 from selenium import webdriver
 import bs4, time, csv, os
 from datetime import datetime
+import pickle
 
 # Variable to keep checks
 drivers = {}
@@ -12,13 +13,21 @@ oddsDict = {}
 # url = input("Enter the URL to scrap: ")		
 url = 'https://www.marathonbet.co.uk/en/live/22723'
 
+
 # for getting the driver instant
 def getDriver():
 	# python3
 	try:
 		options = webdriver.ChromeOptions()
 		options.add_argument('headless')
-		return webdriver.Chrome('./chromedriver',chrome_options=options)
+
+		#########################################################################################################
+		# return webdriver.Chrome('/Users/stakas/Documents/githhub_repos/marathon_scraper/chrome4mac/chromedriver',\
+		#  						chrome_options=options)
+		# return webdriver.Chrome('/home/ubuntu/inplay_tennis_data/marathon_data/chromedriver', chrome_options=options)
+		#########################################################################################################
+
+		return webdriver.Chrome('./chromedriver', chrome_options=options)
 	except:
 		# python2
 		try:
@@ -31,13 +40,13 @@ def getDriver():
 
 
 #Starting the main window and getting the url.
-print('Getting the window\n\n')
+print('Getting the window\n')
 driver = getDriver()
 try:
 	driver.get(url)
 except Exception as e:
 	raise 'Internet issue connect and try again'
-print('\nGot the window\n\n')
+print('Got the window\n')
 
 
 #clicking on the the fraction button on the settings
@@ -46,6 +55,20 @@ driver.find_element_by_xpath('//*[@id="settingsDropdown"]/div[2]/div[1]/table/tb
 driver.find_element_by_xpath('//*[@id="settingsDropdown"]/div[2]/div[5]/button').click()
 
 
+##################################################################################
+# #for checking does the file exist and getting its filePath
+# def getFile(fileName, eventTitle):
+# 	# checks if a folder with the event name exists or not
+# 	filePath = eventTitle + '/' + fileName
+
+# 	if not os.path.exists(filePath):
+# 		os.makedirs(filePath)
+# 	# returning the file path and boolean value that is the files ex in ths folder
+# 	return os.path.exists(filePath), filePath
+##################################################################################
+
+
+##################################################################################
 #for checking does the file exist and getting its filePath
 def getFile(fileName, eventTitle):
 	# checks if a folder with the event name exists or not
@@ -54,11 +77,35 @@ def getFile(fileName, eventTitle):
 	# returning the file path and boolean value that is the files ex in ths folder
 	filePath = eventTitle + '/' + fileName
 	return os.path.exists(filePath), filePath
+##################################################################################
+
+# appendind data to csv file
+def pickleData(playerName, odds, score,  leftData, rightData , eventTitle, fields, indicator):
+
+	fileName = playerName[0] + '_' + playerName[1]
+	isFileExists, filePath = getFile(fileName, eventTitle)
+
+	out = {}
+	out['playerName'] = playerName
+	out['odds'] = odds
+	out['score'] = score
+	out['leftData'] = leftData
+	out['leftData'] = leftData
+	out['rightData'] = rightData
+	out['eventTitle'] = eventTitle
+	out['fields'] = fields
+	# **************************************
+	if indicator!=None:
+		out['dot'] = playerName[indicator-1]
+	# **************************************
+
+	with open(os.path.join(filePath, str(1000*int(time.time())) + '.pickle'), 'wb') as handle:
+		pickle.dump(out, handle, protocol=pickle.HIGHEST_PROTOCOL)
 
 
 # appendind data to csv file 
 def appendInCsv(playerName, odds, score,  leftData, rightData , eventTitle, fields, indicator):
-	print("Appening in CSV File\n\n\n")
+	print("\nAppening in CSV File")
 	# Creating the file inside the folder 
 	fileName = ' and '.join(playerName[0].split('/')) + ' vs ' + ' and '.join(playerName[1].split('/')) + '.csv'
 	isFileExists , filePath = getFile(fileName, eventTitle)
@@ -72,8 +119,6 @@ def appendInCsv(playerName, odds, score,  leftData, rightData , eventTitle, fiel
 	# writing the the data if the file doesn't exists
 	if not isFileExists:
 		#writing for the writer
-
-
 
 		csvWriter.writerow(['Event Detail : ', eventTitle])
 		csvWriter.writerow(['Players Name : ', playerName[0], playerName[1]])
@@ -89,9 +134,11 @@ def appendInCsv(playerName, odds, score,  leftData, rightData , eventTitle, fiel
 			csvWriter.writerow(['Odds : ', odds])
 		csvWriter.writerow([''])
 
+		#**********************************************************
 		if indicator!=None:
-			csvWriter.writerow([playerName[indicator-1],'(dot)'])
+			csvWriter.writerow(['Turn : ',playerName[indicator-1]])
 		csvWriter.writerow([''])
+		#**********************************************************
 
 		#writing for the dictwriter
 		csvDictWriter.writeheader()
@@ -110,10 +157,12 @@ def appendInCsv(playerName, odds, score,  leftData, rightData , eventTitle, fiel
 		if odds!=None:
 			csvWriter.writerow(['Odds : ', odds])
 		csvWriter.writerow([''])
-
+		
+		#**********************************************************
 		if indicator!=None:
-			csvWriter.writerow([playerName[indicator-1],'(dot)'])
+			csvWriter.writerow(['Turn : ',playerName[indicator-1]])
 		csvWriter.writerow([''])
+		#**********************************************************
 
 		#writing for the dictwriter
 		csvDictWriter.writeheader()
@@ -124,12 +173,13 @@ def appendInCsv(playerName, odds, score,  leftData, rightData , eventTitle, fiel
 		csvWriter.writerow([''])
 
 	csvFile.close()
-	print('Appened!')
+	print('Appened!\n')
+
 
 # Getting the statistic o each match when ever there is a change
 def getStats(matchId, eventTitle, matchTitle):
  	# Url for getting th stats
-	print("\n\nGetting the stats....\n\n")
+	print("Getting the stats for {}".format(matchTitle))
 	statsUrl = "https://www.marathonbet.co.uk/en/live/animation/statistic.htm?treeId=" + matchId
 	
 
@@ -163,7 +213,7 @@ def getStats(matchId, eventTitle, matchTitle):
 	# Gets the bs4 object or the scarpping the stats 	
 	stats = bs4.BeautifulSoup(drivers[eventTitle]['driver'].page_source,'html.parser')
 
-	# trying to getthe data ofthe player
+	#trying to getthe data ofthe player
 	#Getting the playerNames
 	try:
 		leftPlayer = stats.find(class_='left ellipsis-simple').get_text()
@@ -223,26 +273,11 @@ def getStats(matchId, eventTitle, matchTitle):
 
 #getting the details of the match like score and name of the player
 def getMatchDetails(match):
-	print("\nGetting Match Details... \n\n\n")
 	matchId = match.get_attribute('data-event-treeid')
 	playerName = [name.text for name in match.find_elements_by_class_name('live-today-member-name')]
 	score = match.find_element_by_class_name('result-row').text
 	matchTitle = playerName[0]+' vs ' + playerName[1]
-
-
-	# ****************dot indicator******************
-	indicator = None
-	allIndicatorTags = match.find_elements_by_class_name('sport-indicator')
-	try:
-		allIndicatorTags[0].find_element_by_tag_name('img')
-		indicator=1
-	except:
-		allIndicatorTags[1].find_element_by_tag_name('img')
-		indicator=2
-	else:
-		pass
-	# *************************************************
-
+	print("Getting Match Details for {}".format(matchTitle))
 
 	#Getting the last score to check if the score has changed or not
 	isOddChange = False
@@ -253,6 +288,20 @@ def getMatchDetails(match):
 	except Exception as e:
 		odds = None
 		pass
+
+	#*****************************dot indicator****************************
+	indicator = None
+	allIndicatorTags = match.find_elements_by_class_name('sport-indicator')
+	try:
+		allIndicatorTags[0].find_element_by_tag_name('img')
+		indicator=1
+	except:
+		allIndicatorTags[1].find_element_by_tag_name('img')
+		indicator=2
+	else:
+		pass
+	#**********************************************************************
+
 
 	#Checks if the score and odds have changed
 
@@ -277,30 +326,34 @@ def getMatchDetails(match):
 def start():
 	#Initiating the program.
 	checktime = datetime.now()
-	while True:s
+	while True:
 		eventTitles = []
 		matchTitles = []
 		#Scrapping
+
 		try:
 			allEvents = driver.find_elements_by_class_name('category-container')	
 		except Exception as e:
+			print('{}'.format(e))
 			# driver.refresh()
 			# allEvents = driver.find_elements_by_class_name('category-container')	
 			print("Can't find any match\nSleeping for 30 sec! till any match starts...")
 			time.sleep(30)
 			continue
 
-	
-
 		#Scrapping the data of eacch Tournament
 		for event in allEvents:
 			try:
 				eventTitle = event.find_element_by_class_name('category-label-td').text
+
+				if 'ITF' in eventTitle or 'Doubles' in eventTitle or ('ATP' not in eventTitle and 'WTA' not in eventTitle):
+					continue
+				print('Getting matches from the event {}'.format(eventTitle))
 				eventId = event.find_element_by_class_name('category-content').get_attribute('id')
 				allMatches = event.find_elements_by_xpath('//*[@id="{}"]/div/table/tbody'.format(eventId))
 			
 				eventTitles.append(eventTitle)
-				#Gettinhg the data of each match
+				#Getting the data of each match
 				for match in allMatches:
 					#scraping the details
 					try:
@@ -309,39 +362,41 @@ def start():
 						continue
 					matchTitle = playerName[0]+' vs ' + playerName[1]
 				
-					print(matchId)
-					print(playerName)
-					print(odds)
-					print(score)
+					# print(matchId, playerName, odds, score)
 
 					matchTitles.append(matchTitle)
 
 
 					#Trying to get the stats of the match if the score has changed
 					fields, leftData, rightData, isStatsChange = getStats(matchId, eventTitle, matchTitle)
-					
+
 					#checking if statistic or score or odd any of them has change or not.
 					if isStatsChange or isScoreChange or isOddChange:
 						#Saving the data in CSV
+
+						#########################################################################################
+						# pickleData(playerName, odds, score, leftData, rightData, eventTitle, fields, indicator)
+						#########################################################################################
+						
 						appendInCsv(playerName, odds, score, leftData, rightData, eventTitle, fields, indicator)
+
 					#Updating the last score
 					scores[matchTitle] = score
 					oddsDict[matchTitle] = odds
-					# time.sleep(1)
 			except:
 				continue
 		
-		#Checks if any match has ended after every one hour so if can close the tab bor window for it.
+		#Checks if any match has ended after every half an hour so it can close the tab for it.
 		if (datetime.now() - checktime).seconds > 1800:
 			print("Checking to kill")
-
-			for event in drivers.keys():
+			keyz = list(drivers.keys())
+			for event in keyz:
 				try:
 					if not event in eventTitles:
 						driverClose = drivers[event].get('driver', None)
 						if driverClose:
 							for match in drivers[event]['tabs'].keys():
-								tab = drivers[event]['tabs'].get(match,None)
+								tab = drivers[event]['tabs'].get(match, None)
 								if tab:
 									driverClose.switch_to_window(tab)
 									print('Closing a tab')
@@ -349,13 +404,13 @@ def start():
 									del oddsDict[match]
 									del scores[match]
 							del drivers[event]
-							del statistic[event]	
-							
+							del statistic[event]
+
 					else:
 						for match in drivers[event]['tabs'].keys():
 							if not match in matchTitles:
 								driverClose = drivers[event].get('driver', None)
-								tab = drivers[event]['tabs'].get(match,None)
+								tab = drivers[event]['tabs'].get(match, None)
 								if tab and driverClose:
 									driverClose.switch_to_window(tab)
 									print('Closing a tab')
@@ -366,7 +421,7 @@ def start():
 									del scores[match]
 				except Exception as e:
 					print('Some error')
-					continue 
-				
+					continue
+
 			checktime = datetime.now()
 start()
